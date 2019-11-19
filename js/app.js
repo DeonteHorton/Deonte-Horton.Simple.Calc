01b3744f33ee218c2a9ad9ebfaa8f7a2a6780b9b
DeonteHorton <deontedeshuanhorton@gmail.com>
2019-11-19 10:05:40 -0600

    added eventlistener to all-clear

--- js/app.js
+++ js/app.js
@@ -1,92 +1,108 @@
 /*
-    Simple cal
-        When buttons are pressed, store numbers
-        when first number is pressed, store in first number
-        when second number is pressed, store in current number
-        when operator is pressed, save operator and to first number
+    Simple calc
+
+        when current number is pressed, store in current number
+        when operator is pressed, save operator and store current number in old number and reset current number
+
+        Then convert numbers from string to interger
         
         when equal button is clicked
-            if input includes(+)
-                run addition function
-            else if input includes(-)
-                run subtraction function
-            else if input includes(divide symbol)
-                run division function
-            else if input includes (*)
-                run multiplication function
+        if the case was '+'
+            do addition operation
+        if the case was '-'
+            do addition operation
+        if the case was '*'
+            do addition operation
+        if the case was '/'
+            do addition operation
+         then DISPLAY "result"
+         if continuing to do operation against result
+         reset old number
+         have current number equal result
+         
+    Completed
+        NEW!
+        do multiple operations
+        accept more than 1 right hand number
+        if there is more than one right hand number 
+            do the left hand number and the first right hand number
+            then do 
 */
 
     var numbers = document.querySelectorAll('.num');
     var operator = document.querySelectorAll(".operator");
     var screen = document.getElementById("display");
+    var clear = document.getElementsByClassName("all-clear")
     var equal = document.getElementById("equal-sign");
     var curNum = ""; // Current number
     var oldNum = ""; // First number
     var result;
 
-    function clr() 
-    { 
-        curNum = "";
-        oldNum = "";
-        result = "";
-        document.getElementById("display").value = "0"; 
+    for (let i = 0; i < clear.length; i++) {
+        //for addeventlistener function, there is no way to pass a param
+        clear[i].addEventListener('click',function () {
+            curNum = "";
+            oldNum = "";
+            result = "";
+            document.getElementById("display").value = "0";
+        })
+        
     }
+    
 
     //get current number
     for (let i = 0; i < numbers.length; i++) {
-        numbers[i].addEventListener('click',function (result) {
-           if (result) {
+        numbers[i].addEventListener('click',function () {
+
             curNum += this.getAttribute('data-value');
             result = "";
             console.log(curNum);
             
-           } else {
-            curNum = this.getAttribute('data-value');
-            console.log(curNum);
-            
-           }
-           screen.value = curNum;
+            screen.value = curNum;
         })
     }
     
-    //everytime an operator is pressed, it stores the curNum in oldNum and resets if its two digit or more
+    //everytime an operator is pressed, it stores the curNum in oldNum and resets 
     for (let i = 0; i < operator.length; i++) {
         operator[i].addEventListener('click',function () {
             oldNum = curNum;
             curNum = "";
             operator = this.getAttribute("data-op");
+            console.log(operator);
         })
     }
 
     function solve() {
-    oldNum = parseFloat(oldNum);
-    curNum = parseFloat(curNum);
+     oldNum = parseFloat(oldNum);
+     curNum = parseFloat(curNum);
 
-    // Perform operation
-    switch (operator) {
-        case "+":
-        result = oldNum + curNum;
-        break;
+     // Perform operation
+     switch (operator) {
+         case "+":
+         result = oldNum + curNum;
+         break;
 
-        case "-":
-        result = oldNum - curNum;
-        break;
+         case "-":
+         result = oldNum - curNum;
+         break;
 
-        case "*":
-        result = oldNum * curNum;
-        break;
+         case "*":
+         result = oldNum * curNum;
+         break;
 
-        case "/":
-        result = oldNum / curNum;
-        break;
+         case "/":
+         result = oldNum / curNum;
+         break;
 
-        // If equal is pressed without an operator, keep number and continue
-        default:
-        result = curNum;
+         // If equal is pressed without an operator, keep number and continue
+         default:
+         result = curNum;
     }
+
     screen.value = result;
-    //resets and get the result 
+    //resets old number and changes result to current number
     oldNum = 0;
     curNum = result;
         
 }
+
